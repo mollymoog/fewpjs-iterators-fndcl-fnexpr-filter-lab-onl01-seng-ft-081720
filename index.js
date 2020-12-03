71be6023662e4db91cec0ea73800c732a58a07f6
mollymoog <mmkover@gmail.com>
2020-12-02 19:26:00 -0500

    commit tree 781877accbc87e31ec5e436c6410f94c3fac5b02

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+
+function findMatching(drivers, string) {
+    return drivers.filter( d => d.toUpperCase() == string.toUpperCase() )
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter( d => d.startsWith(string))
+}
+
+function matchName(drivers, string) {
+    return drivers.filter( d => d.name == string )
+}
